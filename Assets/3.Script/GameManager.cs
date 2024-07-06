032ddf7873659641ce0fa45a268dcc6a221a6089
cho1124 <122534567+cho1124@users.noreply.github.com>
2024-07-07 00:57:55 +0900

    [Update] GameManager set
    쓰고싶으신 분들 쓰세영

---
+++ Assets/3.Script/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
