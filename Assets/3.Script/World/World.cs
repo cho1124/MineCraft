a4bd676f8f94842d3803b6db4e67291551e2dd9b
zzangmin <wjdals1212gh@gmail.com>
2024-07-10 00:24:55 +0900

    [Jotbug] Fixing
    fk

--- Assets/3.Script/World/World.cs
+++ Assets/3.Script/World/World.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -94,6 +95,9 @@ public class World : MonoBehaviour
 
     void GenerateWorld()
     {
+        PerlinWorm perlinWorm = new PerlinWorm();
+        List<Vector3Int> wormPositions = perlinWorm.CreateWorm(VoxelData.worldSizeInBlocks / 2, VoxelData.worldSizeInBlocks / 2);
+
 
         for (int x = VoxelData.worldSizeInChunks / 2 - VoxelData.viewDistanceInChunks / 2; x < VoxelData.worldSizeInChunks / 2 + VoxelData.viewDistanceInChunks / 2; x++)
         {
@@ -353,3 +357,299 @@ public class BlockType
 
 }
 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 spawnPoint;
+
+    public int seed;
+    public BiomeAttribute biome;
+
+    public Material material;
+    public BlockType[] blockTypes;
+    public GameObject ItemBlock;
+
+    public Chunk[,] chunks = new Chunk[VoxelData.worldSizeInChunks, VoxelData.worldSizeInChunks];
+    List<ChunkCoord> activeChunks = new List<ChunkCoord>();
+
+    public ChunkCoord playerChunkCoord;
+    ChunkCoord playerLastChunkCoord;
+
+    List<ChunkCoord> chunksToCreate = new List<ChunkCoord>();
+    private bool isCreatingChunks;
+
+    public GameObject debugScreen;
+
+    private List<Vector3Int> wormPaths = new List<Vector3Int>();
+    private void Start()
+    {
+        UnityEngine.Random.InitState(seed);
+
+        GenerateWorld();
+        playerLastChunkCoord = GetChunkCoordFromVector3(player.transform.position);
+    }
+
+    private void Update()
+    {
+        playerChunkCoord = GetChunkCoordFromVector3(player.position);
+
+        if (!playerChunkCoord.Equals(playerLastChunkCoord))
+            CheckViewDistance();
+
+        if (chunksToCreate.Count > 0 && !isCreatingChunks)
+            StartCoroutine(CreateChunks_co());
+
+        if (Input.GetKeyDown(KeyCode.F3))
+            debugScreen.SetActive(!debugScreen.activeSelf);
+    }
+
+    ChunkCoord GetChunkCoordFromVector3(Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x / VoxelData.ChunkWidth);
+        int z = Mathf.FloorToInt(pos.z / VoxelData.ChunkWidth);
+        return new ChunkCoord(x, z);
+    }
+
+    public Chunk GetChunkFromVector3(Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x / VoxelData.ChunkWidth);
+        int z = Mathf.FloorToInt(pos.z / VoxelData.ChunkWidth);
+        return chunks[x, z];
+    }
+
+    void GenerateWorld()
+    {
+        PerlinWorm perlinWorm = new PerlinWorm();
+
+        for (int x = 0; x < VoxelData.worldSizeInChunks; x++)
+        {
+            for (int z = 0; z < VoxelData.worldSizeInChunks; z++)
+            {
+                Chunk chunk = new Chunk(new ChunkCoord(x, z), this, false);
+                chunks[x, z] = chunk;
+                activeChunks.Add(new ChunkCoord(x, z));
+                chunksToCreate.Add(new ChunkCoord(x, z));
+
+                int numWorms = Random.Range(1, 4);
+                for (int i = 0; i < numWorms; i++)
+                {
+                    int startY = Random.Range(0, VoxelData.ChunkHeight);
+                    List<Vector3Int> wormPositions = perlinWorm.CreateWorm(x, z, startY);
+                    foreach (var pos in wormPositions)
+                    {
+                        Vector3Int localPos = new Vector3Int(pos.x - x * VoxelData.ChunkWidth,
+                                                             pos.y,
+                                                             pos.z - z * VoxelData.ChunkWidth);
+                        if (chunk.IsVoxelInChunk(localPos.x, localPos.y, localPos.z))
+                        {
+                            chunk.voxelMap[localPos.x, localPos.y, localPos.z] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        spawnPoint = new Vector3(VoxelData.worldSizeInBlocks / 2, VoxelData.ChunkHeight - 50f, VoxelData.worldSizeInBlocks / 2);
+        player.position = spawnPoint;
+    }
+
+    IEnumerator CreateChunks_co()
+    {
+        isCreatingChunks = true;
+
+        while (chunksToCreate.Count > 0)
+        {
+            ChunkCoord chunkCoord = chunksToCreate[0];
+            chunksToCreate.RemoveAt(0);
+            chunks[chunkCoord.x, chunkCoord.z].Init();
+            yield return null;
+        }
+
+        isCreatingChunks = false;
+    }
+
+    void CheckViewDistance()
+    {
+        ChunkCoord coord = GetChunkCoordFromVector3(player.position);
+        playerLastChunkCoord = playerChunkCoord;
+
+        List<ChunkCoord> previouslyActiveChunks = new List<ChunkCoord>(activeChunks);
+
+        for (int x = coord.x - VoxelData.viewDistanceInChunks; x < coord.x + VoxelData.viewDistanceInChunks; x++)
+        {
+            for (int z = coord.z - VoxelData.viewDistanceInChunks; z < coord.z + VoxelData.viewDistanceInChunks; z++)
+            {
+                if (IsChunkInWorld(new ChunkCoord(x, z)))
+                {
+                    if (chunks[x, z] == null)
+                    {
+                        chunks[x, z] = new Chunk(new ChunkCoord(x, z), this, false);
+                        chunksToCreate.Add(new ChunkCoord(x, z));
+                    }
+                    else if (!chunks[x, z].isActive)
+                    {
+                        chunks[x, z].isActive = true; // 没农 劝己拳
+                    }
+
+                    activeChunks.Add(new ChunkCoord(x, z));
+                }
+
+                for (int i = 0; i < previouslyActiveChunks.Count; i++)
+                {
+                    if (previouslyActiveChunks[i].Equals(new ChunkCoord(x, z)))
+                        previouslyActiveChunks.RemoveAt(i);
+                }
+            }
+        }
+
+        foreach (ChunkCoord c in previouslyActiveChunks)
+        {
+            chunks[c.x, c.z].isActive = false; // 没农 厚劝己拳
+        }
+    }
+
+    public bool CheckForVoxel(Vector3 pos)
+    {
+        ChunkCoord thisChunk = new ChunkCoord(pos);
+        if (!IsChunkInWorld(thisChunk) || pos.y < 0 || pos.y >= VoxelData.ChunkHeight)
+        {
+            return false;
+        }
+
+        Chunk chunk = chunks[thisChunk.x, thisChunk.z];
+
+        if (chunk != null && chunk.isVoxelMapPopulated)
+        {
+            int xCheck = Mathf.FloorToInt(pos.x) - (thisChunk.x * VoxelData.ChunkWidth);
+            int yCheck = Mathf.FloorToInt(pos.y);
+            int zCheck = Mathf.FloorToInt(pos.z) - (thisChunk.z * VoxelData.ChunkWidth);
+
+            if (chunk.IsVoxelInChunk(xCheck, yCheck, zCheck))
+            {
+                return blockTypes[chunk.voxelMap[xCheck, yCheck, zCheck]].isSolid;
+            }
+        }
+
+        return blockTypes[GetVoxel(pos)].isSolid;
+    }
+
+    public byte GetVoxel(Vector3 pos)
+    {
+        int yPos = Mathf.FloorToInt(pos.y);
+
+        if (!IsVoxelInWorld(pos))
+            return 0;
+
+        if (yPos == 0)
+            return 1;
+
+        foreach (var wormPos in wormPaths)
+        {
+            if (RoundPos(pos) == wormPos)
+            {
+                return 0; // 后 傍埃
+            }
+        }
+
+        int terrainHeight = Mathf.FloorToInt(biome.terrainHeight * Noise.Get2DPerlin(new Vector2(pos.x, pos.z), 0, biome.terrainScale)) + biome.solidGroundHeight;
+
+        byte voxelValue = 0;
+
+        if (yPos == terrainHeight)
+        {
+            voxelValue = 3; // grass
+        }
+        else if (yPos < terrainHeight && yPos > terrainHeight - (int)UnityEngine.Random.Range(1, 10))
+        {
+            voxelValue = 5; // dirt
+        }
+        else if (yPos > terrainHeight)
+        {
+            return 0; // air
+        }
+        else
+        {
+            voxelValue = 2; // stone
+        }
+
+        if (voxelValue == 2)
+        {
+            foreach (Lode lode in biome.lodes)
+            {
+                if (yPos > lode.minHeight && yPos < lode.maxHeight)
+                {
+                    if (Noise.Get3DPerlin(pos, lode.noiseOffset, lode.scale, lode.threshold))
+                    {
+                        voxelValue = lode.blockID;
+                    }
+                }
+            }
+        }
+
+        return voxelValue;
+    }
+
+    bool IsChunkInWorld(ChunkCoord coord)
+    {
+        return coord.x >= 0 && coord.x < VoxelData.worldSizeInChunks &&
+               coord.z >= 0 && coord.z < VoxelData.worldSizeInChunks;
+    }
+
+    public bool IsVoxelInWorld(Vector3 pos)
+    {
+        return pos.x >= 0 && pos.x < VoxelData.worldSizeInBlocks &&
+               pos.y >= 0 && pos.y < VoxelData.ChunkHeight &&
+               pos.z >= 0 && pos.z < VoxelData.worldSizeInBlocks;
+    }
+
+    Vector3 RoundPos(Vector3 v3)
+    {
+        return new Vector3(Mathf.Floor(v3.x + 0.5f), Mathf.Floor(v3.y + 0.5f), Mathf.Floor(v3.z + 0.5f));
+    }
+}
+
+
+
+[System.Serializable]
+
+public class BlockType
+{
+    public string blockName;
+    public bool isSolid;
+
+    [Header("Texture IDs")]
+    public int topFaceTexture;
+    public int frontFaceTexture;
+    public int backFaceTexture;
+    public int leftFaceTexture;
+    public int rightFaceTexture;
+    public int bottomFaceTexture;
+
+
+    public int GetTextureID(int faceIndex)
+    {
+        switch (faceIndex)
+        {
+            case 0: return backFaceTexture;
+            case 1: return frontFaceTexture;
+            case 2: return topFaceTexture;
+            case 3: return bottomFaceTexture;
+            case 4: return leftFaceTexture;
+            case 5: return rightFaceTexture;
+
+            default:
+                Debug.Log("Error in GetTextureID; invalid face index"); ;
+                return 0;
+        }
+
+    }
+
+
+
+
+}
--- Assets/PerlinWorms/PerlinWorm.cs
+++ Assets/PerlinWorms/PerlinWorm.cs
@@ -1,49 +1,24 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class PerlinWorm : MonoBehaviour
+public class PerlinWorm
 {
     public int maxWormLength = 80;
     public float resolution = 1.0f;
     public int seed = 123;
+    public int wormWidth = 2; // 지렁이의 너비를 정의합니다.
 
-    private void Start()
+    public List<Vector3Int> CreateWorm(int cx, int cz, int startY)
     {
-        List<Dictionary<string, bool>> worms = CreateWorm(0, 0);
-        foreach (var worm in worms)
-        {
-            foreach (var kvp in worm)
-            {
-                Debug.Log($"{kvp.Key}: {kvp.Value}");
-            }
-        }
-    }
+        float sx = Random.Range(0, VoxelData.ChunkWidth);
+        float sy = startY;
+        float sz = Random.Range(0, VoxelData.ChunkWidth);
+        int ammount = 10;
 
-    public static Vector3 RoundPos(Vector3 v3)
-    {
-        return new Vector3(Mathf.Floor(v3.x + 0.5f), Mathf.Floor(v3.y + 0.5f), Mathf.Floor(v3.z + 0.5f));
-    }
-
-    public static Vector3 ConvertChGridToReal(int cx, int cz, float x, float y, float z, bool toBlockInstead)
-    {
-        float scale = toBlockInstead ? 1 : 4;
-        return new Vector3((x + 8 * cx), y, (z + 8 * cz)) * scale;
-    }
-
-    public List<Dictionary<string, bool>> CreateWorm(int cx, int cz)
-    {
-        float sx = Random.Range(0, 8); // Random.Range(0, 8)
-        float sy = Random.Range(0, 128);
-        float sz = Random.Range(0, 8); // Random.Range(0, 8)
-        int ammount = 10;//Random.Range(0, 10);  // Random.Range(0, 4)
-
-        List<Dictionary<string, bool>> worms = new List<Dictionary<string, bool>>();
-        Debug.Log($"Branches: {ammount}");
+        List<Vector3Int> wormPositions = new List<Vector3Int>();
 
         for (int ci = 1; ci <= ammount; ci++)
         {
-            Color color = Random.ColorHSV();
-            Dictionary<string, bool> wormData = new Dictionary<string, bool>();
             Vector3 startPosition = ConvertChGridToReal(cx, cz, sx, sy, sz, true);
             Transform wormTransform = new GameObject().transform;
             wormTransform.position = startPosition;
@@ -54,29 +29,48 @@ public class PerlinWorm : MonoBehaviour
                 float x = Mathf.PerlinNoise((wormTransform.position.x / resolution) + 0.1f, seed + ci) + 0.01f;
                 float y = Mathf.PerlinNoise((wormTransform.position.y / resolution) + 0.1f, seed + ci) + 0.01f;
                 float z = Mathf.PerlinNoise((wormTransform.position.z / resolution) + 0.1f, seed + ci) + 0.01f;
-                wormTransform.rotation *= Quaternion.Euler(x * (Random.Range(-48,45) + ci), y * (Random.Range(-48, 45) + ci), z * (Random.Range(-48, 45) + ci));
-                //wormTransform.rotation *= Quaternion.Euler(x * (1 + ci), y * (1 + ci), z * (1 + ci));
+                wormTransform.rotation *= Quaternion.Euler(x * (Random.Range(-48, 45) + ci), y * (Random.Range(-48, 45) + ci), z * (Random.Range(-48, 45) + ci));
                 wormTransform.position += wormTransform.forward * -resolution;
 
-                Vector3 roundedPos = RoundPos(wormTransform.position);
-                string posKey = roundedPos.ToString();
-                wormData[posKey] = false;
-
-                GameObject part = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                part.transform.position = roundedPos;
-                part.transform.localScale = new Vector3(1, 1, 1);
-                part.GetComponent<Renderer>().material.color = color;
-                part.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
-                if (i == 1)
+                Vector3Int roundedPos = Vector3Int.FloorToInt(wormTransform.position);
+                if (IsInWorldBounds(roundedPos))
                 {
-                    part.GetComponent<Renderer>().material.SetColor("_EmissionColor", color);
+                    AddWormWidthPositions(roundedPos, wormPositions);
                 }
-                part.transform.parent = transform;
             }
-
-            worms.Add(wormData);
         }
 
-        return worms;
+        return wormPositions;
+    }
+
+    private void AddWormWidthPositions(Vector3Int centerPos, List<Vector3Int> wormPositions)
+    {
+        for (int x = -wormWidth; x <= wormWidth; x++)
+        {
+            for (int y = -wormWidth; y <= wormWidth; y++)
+            {
+                for (int z = -wormWidth; z <= wormWidth; z++)
+                {
+                    Vector3Int pos = centerPos + new Vector3Int(x, y, z);
+                    if (IsInWorldBounds(pos) && !wormPositions.Contains(pos))
+                    {
+                        wormPositions.Add(pos);
+                    }
+                }
+            }
+        }
+    }
+
+    public static Vector3 ConvertChGridToReal(int cx, int cz, float x, float y, float z, bool toBlockInstead)
+    {
+        float scale = toBlockInstead ? 1 : 4;
+        return new Vector3((x + 8 * cx), y, (z + 8 * cz)) * scale;
+    }
+
+    private bool IsInWorldBounds(Vector3Int pos)
+    {
+        return pos.x >= 0 && pos.x < VoxelData.worldSizeInBlocks &&
+               pos.y >= 0 && pos.y < VoxelData.ChunkHeight &&
+               pos.z >= 0 && pos.z < VoxelData.worldSizeInBlocks;
     }
 }
