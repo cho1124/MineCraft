ecabcaa4ff36eb0da35ee1884b7dd34ff5a536d1
한유진 <bittersweeety@gmail.com>
2024-07-09 09:56:52 +0900

    [update] 고양이 테스트중

---
+++ Assets/3.Script/Cat/Cat_State.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//    public interface ICatState
+//    {
+//        void EnterState(Cat cat);
+//        void UpdateState(Cat cat);
+//        void ExitState(Cat cat);
+//    }
+//
+//public class StandingState : ICatState
+//{
+//    public void EnterState(Cat cat)
+//    {
+//        // 서 있는 상태에 진입할 때 호출되는 코드
+//        cat.animator.Play("Standing");
+//    }
+//
+//    public void UpdateState(Cat cat)
+//    {
+//        // 서 있는 동안 수행할 로직
+//        if (Input.GetKeyDown(KeyCode.W))
+//        {
+//            cat.TransitionToState(cat.walkingState);
+//        }
+//        else if (Input.GetKeyDown(KeyCode.Space))
+//        {
+//            cat.TransitionToState(cat.jumpingState);
+//        }
+//    }
+//
+//    public void ExitState(Cat cat)
+//    {
+//        // 서 있는 상태를 떠날 때 호출되는 코드
+//    }
+//}
+//
+
+
+
+
+
+
+
+
