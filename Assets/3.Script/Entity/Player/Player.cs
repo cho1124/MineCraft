7cf7942c0adefef16bcfe95362296164d2bac43e
cho1124 <122534567+cho1124@users.noreply.github.com>
2024-07-11 09:32:40 +0900

    [Update] data_test
    테스트 입니다

--- Assets/3.Script/Entity/Player/Player.cs
+++ Assets/3.Script/Entity/Player/Player.cs
@@ -5,4 +5,21 @@ using UnityEngine;
 public class Player : Entity
 {
     //플레이어는 단순히 엔티티의 속성을 받는 것 뿐만이 아닌 좀 더 다양한 속성을 구현해야 해요. -> 플레이어만이 가지는 포만도, 목마름, 경험치 등을 여기서 구현하면 되지 않을까요?
+    private float Exp;
+
+
+    public void Gethealth()
+    {
+        Health = 2;
+    }
+
+    public float GetEXP()
+    {
+        return Exp++;
+    }
+
+
+
+
+
 }
