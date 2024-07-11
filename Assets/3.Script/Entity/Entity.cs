8bfc040781ac2a8a92bc660a96704d3a7b73a2e0
Under283 <jihawlgk0824@gmail.com>
2024-07-12 04:00:13 +0900

    [update] entity & item default data structure

--- Assets/3.Script/Entity/Entity.cs
+++ Assets/3.Script/Entity/Entity.cs
@@ -22,35 +22,23 @@ public class Entity : MonoBehaviour
     
     private float defense_base;
     private float defense_current;
-    
-    private float weight_max;
-    private float weight_base;
-    private float weight_current;
 
     private float speed_rate;
     private float speed_walk;
     private float speed_sprint;
     private float jump_height;
 
-    private Hand L_hand;
-    private Hand R_hand;
-
-    protected GameObject[] equipments; // 투구 갑옷 바지 신발 왼손 오른손
-    private GameObject[] inventory;
-
     public void uptate_status()
     {
-        health_max = 100 * (1 + VIT * 0.01f);
-        posture_max = 100 * (1 + weight_current * 0.01f);
-
+        //health_max = 100 * (1 + VIT * 0.01f);
+        //posture_max = 100 * (1 + weight_current * 0.01f);
+        //...
         //올려둔 공식 참조
     }
 
     public void OnDamage(float damage)
     {
     }
-
-
 //나머지는 커스텀 해서 구현 할 것
 }
 
@@ -63,7 +51,20 @@ public class Hand
     private string[] actions;
 }
 
-public class Equipment
+public class Entity_Humanoid_Data : Entity
 {
+    private float weight_max;
+    private float weight_base;
+    private float weight_current;
 
+    private Hand L_hand;
+    private Hand R_hand;
+
+    protected GameObject[] equipments; // 투구 갑옷 바지 신발 왼손 오른손
+    private GameObject[] inventory;
+}
+
+public class Entity_NonHumanoid_Data : Entity
+{
+    private string[] actions;
 }
