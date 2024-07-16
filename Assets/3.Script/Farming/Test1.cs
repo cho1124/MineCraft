d29eee5e377b3fcce7b119a2d056baed217b9422
한유진 <bittersweeety@gmail.com>
2024-07-16 12:23:48 +0900

    [update] farming test

--- Assets/3.Script/Farming/Test1.cs
+++ Assets/3.Script/Farming/Test1.cs
@@ -6,7 +6,7 @@ public class Test1 : MonoBehaviour
 {
     public GameObject melonSeedPrefab;
     public GameObject beafPrefab; // beaf 프리팹을 인스펙터에서 설정
-    public Material leafMaterial;
+    public Material brownLightMaterial;
 
     private Renderer renderer;
     private Collider currentCollider;
@@ -25,22 +25,6 @@ public class Test1 : MonoBehaviour
         // 마우스 왼쪽 클릭 확인
         if (Input.GetMouseButtonDown(0) && currentCollider != null)
         {
-          //  // 충돌한 오브젝트의 메터리얼을 가져옴
-          //  Renderer renderer = currentCollider.gameObject.GetComponent<Renderer>();
-          //
-          //  if (renderer != null && renderer.material == leafMaterial)
-          //  {
-          //      // 충돌한 오브젝트를 melonSeedPrefab으로 변경
-          //      Vector3 position = currentCollider.transform.position;
-          //      Quaternion rotation = currentCollider.transform.rotation;
-          //
-          //      // 기존 오브젝트를 삭제
-          //      Destroy(currentCollider.gameObject);
-          //
-          //      // 새로운 프리팹 인스턴스 생성
-          //      Instantiate(melonSeedPrefab, position, rotation);
-          //  }
-
             // 충돌한 오브젝트가 "Animals" 태그를 가지고 있는지 확인
             if (currentCollider.gameObject.CompareTag("Animals"))
             {
@@ -72,15 +56,13 @@ public class Test1 : MonoBehaviour
             // 충돌한 오브젝트가 "Grass" 태그를 가지고 있는지 확인
             if (currentCollider.gameObject.CompareTag("Ground"))
             {
-                // 충돌한 오브젝트의 위치와 회전 값을 저장
-                Vector3 position = currentCollider.transform.position;
-                Quaternion rotation = currentCollider.transform.rotation;
-
-                // 기존 동물 오브젝트를 삭제
-                Destroy(currentCollider.gameObject);
-
-                // 새로운 beaf 프리팹 인스턴스 생성
-                Instantiate(melonSeedPrefab, position, rotation);
+                //충돌한 오브젝트의 Renderer 컴포넌트를 가져옴
+                renderer = currentCollider.gameObject.GetComponent<Renderer>();
+                if(renderer !=null)
+                {
+                    //메터리얼을 바꿈
+                    renderer.material = brownLightMaterial;
+                }
             }
 
             // 충돌 정보 초기화
