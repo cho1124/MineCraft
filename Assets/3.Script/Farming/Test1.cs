917fbf56a9e476503ca280946c5bcdcabf8e7167
한유진 <bittersweeety@gmail.com>
2024-07-16 15:18:20 +0900

    [update] fix

--- Assets/3.Script/Farming/Test1.cs
+++ Assets/3.Script/Farming/Test1.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Test1 : MonoBehaviour
 {
+    /*
+    (물을 사용하지 않기 위해 애를 쓰며)
+    짜치는 농사 테스트중
+
+    1. grass 태그 붙은 큐브 뽀개면 melonseed 나옴
+    2. ground 태그 붙은 큐브 뽀개면 연한갈색으로 바뀜
+    3. 연한갈색으로 바뀐 큐브를 때리면 "비료를 주었습니다" 로그 뜨면서 진한 갈색으로 바뀜
+    4. 진한 갈색으로 바뀐 큐브는 60f간 아무런 변화가 없으면 다시 연한 갈색으로 바뀜
+    (주기적으로 플레이어가 계속 때려주어야함)
+    5. 300f동안 진한 갈색 큐브가 유지되면 진한 갈색 큐브가 멜론으로 바뀜! 
+
+    [멜론seed 이용방법 고민중]
+    */
+
     public GameObject melonSeedPrefab;
+    public GameObject melonPrefab;
     public GameObject beafPrefab; // beaf 프리팹을 인스펙터에서 설정
     public Material brownLightMaterial;
+    public Material brownDarkMaterial;
 
     private Renderer renderer;
     private Collider currentCollider;
+    private Dictionary<GameObject, Coroutine> fertilizedGrounds = new Dictionary<GameObject, Coroutine>();
 
     private void OnTriggerEnter(Collider other)
     {
@@ -60,13 +77,62 @@ public class Test1 : MonoBehaviour
                 renderer = currentCollider.gameObject.GetComponent<Renderer>();
                 if(renderer !=null)
                 {
-                    //메터리얼을 바꿈
-                    renderer.material = brownLightMaterial;
+                    if (renderer.material == brownLightMaterial)
+                    {
+                        Debug.Log("비료를 주었습니다");
+                        //메터리얼을 바꿈
+                        renderer.material = brownDarkMaterial;
+
+                        if(fertilizedGrounds.ContainsKey(currentCollider.gameObject))
+                        {
+                            StopCoroutine(fertilizedGrounds[currentCollider.gameObject]);
+                            fertilizedGrounds.Remove(currentCollider.gameObject);
+
+                        }
+
+                        //300f 후에 멜론으로 변경
+                        StartCoroutine(ChangeToMelonAfterTime(currentCollider.gameObject, 300f));
+
+                    }
+                    else
+                    {
+                        //연한 갈색으로 변경
+                        renderer.material = brownLightMaterial;
+                    }
                 }
             }
 
             // 충돌 정보 초기화
             currentCollider = null;
+        }
+    }
+
+    private IEnumerator ChangeToMelonAfterTime(GameObject ground, float time)
+    {
+        float elapsedTime = 0f;
+        Renderer groundRenderer = ground.GetComponent<Renderer>();
+
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+
+            // 60f 동안 변화가 없으면 연한 갈색으로 변경
+            if (elapsedTime >= 60f && groundRenderer.material == brownDarkMaterial)
+            {
+                groundRenderer.material = brownLightMaterial;
+                yield break;
+            }
+
+            yield return null;
+        }
+
+        // 300f 후에 멜론으로 변경
+        if (groundRenderer.material == brownDarkMaterial)
+        {
+            Vector3 position = ground.transform.position;
+            Quaternion rotation = ground.transform.rotation;
+            Destroy(ground);
+            Instantiate(melonPrefab, position, rotation);
         }
     }
 }
