c0a0cd116c1db67b8866869e4fe68a12d845af39
한유진 <bittersweeety@gmail.com>
2024-07-17 12:08:11 +0900

    [update] when get attacked, entity's blink in red

--- Assets/3.Script/Entity/Entity.cs
+++ Assets/3.Script/Entity/Entity.cs
@@ -6,6 +6,7 @@ public class Entity : MonoBehaviour
 {
     // 데미지를 입으면 3초간 빨간색으로 깜박거리는 코드 (필요시 삭제수정해주세요) 24.07.16 수정(한유진)
     // 원본 코드 하단에 있어서 필요시 복붙으로 수정 가능합니다.
+    // entity가 공격받을때 빨갛게 변하게 하려고 함 
     // Start is called before the first frame update
     private float maxHealth=100;
     private float health;
@@ -14,16 +15,21 @@ public class Entity : MonoBehaviour
     private float weight;
     private float speed;
 
-    private Renderer entityRenderer;
-    private Color originalColor;
+    private Renderer[] entityRenderer;
+    private Color[] originalColor;
 
     protected virtual void Start()
     {
         health = maxHealth;
-        entityRenderer = GetComponent<Renderer>();
-        if (entityRenderer != null)
+        entityRenderer = GetComponentsInChildren<Renderer>();
+        originalColor = new Color[entityRenderer.Length];
+        for (int i = 0; i < entityRenderer.Length; i++)
         {
-            originalColor = entityRenderer.material.color;
+            originalColor[i] = entityRenderer[i].material.color;
+        }
+        if (entityRenderer.Length==0)
+        {
+            Debug.Log("렌더러 컴포넌트를 찾을 수 없습니다.");
         }
     }
 
@@ -56,13 +62,19 @@ public class Entity : MonoBehaviour
 
         while(elapsedTime<3f)
         {
-            entityRenderer.material.color = isRed ? originalColor : Color.red;
-            isRed = !isRed;
+            for (int i = 0; i < entityRenderer.Length; i++)
+            {
+                entityRenderer[i].material.color = isRed ? originalColor[i] : Color.red;
+            }
+                isRed = !isRed;
             elapsedTime += 0.3f;  //깜박이는 속도
             yield return new WaitForSeconds(0.3f);
         }
-        entityRenderer.material.color = originalColor;
-    }
+        for (int i = 0; i < entityRenderer.Length; i++)
+        {
+            entityRenderer[i].material.color = originalColor[i];
+        }
+        }
 
     protected virtual void OnDie()
     {
