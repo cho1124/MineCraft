f6fd14d541dc0626647cb4b08363538ec802e603
zzangmin <wjdals1212gh@gmail.com>
2024-07-17 21:55:42 +0900

    [Update] Lighting
    but lagging

--- Assets/3.Script/Data/ChunkData.cs
+++ Assets/3.Script/Data/ChunkData.cs
@@ -51,7 +51,7 @@ public class ChunkData
                 {
                     Vector3 voxelGlobalPos = new Vector3(x + position.x, y, z + position.y);
 
-                    map[x, y, z] = new VoxelState(World.Instance.GetVoxel(voxelGlobalPos), this, new Vector3Int(x,y,z);
+                    map[x, y, z] = new VoxelState(World.Instance.GetVoxel(voxelGlobalPos), this, new Vector3Int(x,y,z));
                     //Debug.Log($"Position: {voxelMap[x,y,z]}, Noise Value: {noiseValue}, Voxel Value: {voxelValue}");
 
                     for(int p = 0; p<6; p++)
@@ -74,6 +74,32 @@ public class ChunkData
 
         World.Instance.worldData.AddToModifiedChunkList(this);
 
+
+    }
+    public void ModifyVoxel(Vector3Int pos, byte _id)
+    {
+        if (map[pos.x, pos.y, pos.z].id == _id)
+            return;
+
+        VoxelState voxel = map[pos.x, pos.y, pos.z];
+        BlockType newVoxel = World.Instance.blockTypes[_id];
+
+        byte oldOpacity = voxel.properties.opacity;
+
+        voxel.id = _id;
+
+        if (voxel.properties.opacity != oldOpacity &&
+            (pos.y == VoxelData.ChunkHeight - 1 || map[pos.x, pos.y + 1, pos.z].light == 15))
+        {
+
+            Lighting.CastNaturalLight(this, pos.x, pos.z, pos.y + 1);
+
+        }
+
+        World.Instance.worldData.AddToModifiedChunkList(this);
+
+        if (chunk != null)
+            World.Instance.AddChunkToUpdate(chunk);
 
     }
 
