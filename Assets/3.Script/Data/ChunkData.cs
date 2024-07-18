929b242570275902758dc44cd9189d8ef7d83538
zzangmin <wjdals1212gh@gmail.com>
2024-07-18 10:11:42 +0900

    Revert "[Fixing] Chunk Lighting"
    This reverts commit 659a1f27bbb950d44f5a6adf28262cd7984bf9d1.

--- Assets/3.Script/Data/ChunkData.cs
+++ Assets/3.Script/Data/ChunkData.cs
@@ -21,17 +21,16 @@ public class ChunkData
         }
 
 
+
+
+
+
+
     }
-
-
 
 
     public ChunkData(Vector2Int pos) { position = pos; }
     public ChunkData(int _x, int _y) { x = _x; y = _y; }
-
-
-    [System.NonSerialized]
-    public Chunk chunk;
 
 
     [HideInInspector]
@@ -49,52 +48,17 @@ public class ChunkData
             {
                 for (int z = 0; z < VoxelData.ChunkWidth; z++)
                 {
-                    Vector3 voxelGlobalPos = new Vector3(x + position.x, y, z + position.y);
-
-                    map[x, y, z] = new VoxelState(World.Instance.GetVoxel(voxelGlobalPos), this, new Vector3Int(x,y,z);
+                    map[x, y, z] = new VoxelState(World.Instance.GetVoxel(new Vector3(x + position.x, y, z + position.y)));
                     //Debug.Log($"Position: {voxelMap[x,y,z]}, Noise Value: {noiseValue}, Voxel Value: {voxelValue}");
 
-                    for(int p = 0; p<6; p++)
-                    {
-                        Vector3Int neighbourV3 = new Vector3Int(x, y, z) + VoxelData.faceChecks[p];
-                        if (IsVoxelInChunk(neighbourV3))
-                            map[x, y, z].neighbours[p] = VoxelFromV3Int(neighbourV3);
-                        else
-                            map[x, y, z].neighbours[p] = World.Instance.worldData.GetVoxel(voxelGlobalPos + VoxelData.faceChecks[p]);
-
-
-                    }
 
 
                 }
             }
         }
 
-        Lighting.RecalculateNaturalLight(this);
-
         World.Instance.worldData.AddToModifiedChunkList(this);
 
 
     }
-
-
-    public bool IsVoxelInChunk(int x, int y, int z)
-    {
-        if (x < 0 || x >= VoxelData.ChunkWidth || y < 0 || y >= VoxelData.ChunkHeight || z < 0 || z >= VoxelData.ChunkWidth)
-            return false;
-        else
-            return true;
-
-    }
-
-    public bool IsVoxelInChunk(Vector3Int pos)
-    {
-        return IsVoxelInChunk(pos.x, pos.y, pos.z);
-    }
-
-    public VoxelState VoxelFromV3Int(Vector3Int pos)
-    {
-        return map[pos.x, pos.y, pos.z];
-    }
-
 }
