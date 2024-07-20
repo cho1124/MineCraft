86bac6b8d4f000c39afe9a48853ac9837b03b18f
한유진 <bittersweeety@gmail.com>
2024-07-20 23:21:17 +0900

    [fix] creeper's animation2

--- Assets/3.Script/Mob/Slime.cs
+++ Assets/3.Script/Mob/Slime.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slime : MonoBehaviour
+public class Slime : Monster
 {
     // Start is called before the first frame update
     void Start()
