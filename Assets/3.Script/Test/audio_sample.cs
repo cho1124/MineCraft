a515a74bb8f34a64ebe69a7fd9d15230a3e5c69b
한유진 <bittersweeety@gmail.com>
2024-07-24 17:55:31 +0900

    [UPDATE]

---
+++ Assets/3.Script/Test/audio_sample.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio_sample : MonoBehaviour
+{
+    // 자세하게 읽어보기!
+
+
+
+    public static audio_sample instance = null;
+
+    [Header("Volume")]
+    [Range(0f, 1f)]
+    public float masterVolume = 1f;
+    [Range(0f, 1f)]
+    public float musicVolume = 1f;
+    [Range(0f, 1f)]
+    public float sfxVolume = 1f;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        masterVolume = volume;
+        AudioListener.volume = masterVolume;
+        PlayerPrefs.SetFloat("MasterVolume", masterVolume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicVolume = volume;
+        // Music audio sources volume adjustment logic here
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxVolume = volume;
+        // SFX audio sources volume adjustment logic here
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadSettings()
+    {
+        if (PlayerPrefs.HasKey("MasterVolume"))
+        {
+            SetMasterVolume(PlayerPrefs.GetFloat("MasterVolume"));
+        }
+
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            SetMusicVolume(PlayerPrefs.GetFloat("MusicVolume"));
+        }
+
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            SetSFXVolume(PlayerPrefs.GetFloat("SFXVolume"));
+        }
+    }
+
+
+}
+
