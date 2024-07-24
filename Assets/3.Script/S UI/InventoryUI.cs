3402d2894b006779a1a8ae77387691de4e6bf6d8
JDK <jdkqudy3164@gmail.com>
2024-07-24 10:43:11 +0900

    Fix

--- Assets/3.Script/S UI/InventoryUI.cs
+++ Assets/3.Script/S UI/InventoryUI.cs
@@ -8,19 +8,26 @@ public class InventoryUI : MonoBehaviour
 
     public Slot[] slot_count = null;
     public Inventory inventory_class = null;
-    public Transform slotHolder = null;
-
+    public Transform[] slotHolders = null; // Transform 배열로 변경
 
     private void Start()
     {
-        slot_count = slotHolder.GetComponentsInChildren<Slot>();
+        List<Slot> slots = new List<Slot>();
+
+        // 각 slotHolder에서 Slot 배열을 가져와 리스트에 추가
+        foreach (Transform slotHolder in slotHolders)
+        {
+            slots.AddRange(slotHolder.GetComponentsInChildren<Slot>());
+        }
+
+        slot_count = slots.ToArray();
 
         inventory_class.onchangeitem += RedrawSlotUI;
     }
 
     private void RedrawSlotUI()
     {
-        for (int i = 0; i < slot_count.Length; i++) // Slot_count 크기만큼 실행
+        for (int i = 0; i < slot_count.Length; i++) // slot_count 크기만큼 실행
         {
             slot_count[i].RemoveSlot();
         }
@@ -28,7 +35,6 @@ public class InventoryUI : MonoBehaviour
         for (int i = 0; i < inventory_class.item_list.Count; i++) // 인벤토리 아이템 개수만큼 실행
         {
             slot_count[i].iteminfo_class = inventory_class.item_list[i];
-
             slot_count[i].UpdateSlotUI();
         }
     }
